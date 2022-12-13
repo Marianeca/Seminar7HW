91b6474927838082ff7a6bed76dd5c72c3a81a89
Mar <marianeca.of@gmail.com>
2022-12-13 22:13:13 +0300

    Task47 in progress

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = new Random().Next(200) / 10.0 - 10;
+        array[i, j] = Math.Round(new Random().NextDouble() * 19.0 - 9, 1);
         Console.Write($"{array[i, j]} ");
     }
     Console.WriteLine();
