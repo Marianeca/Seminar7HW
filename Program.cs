bd55c2adeacbb328b11a72df5db06815642d1bc2
Mar <marianeca.of@gmail.com>
2022-12-13 17:19:16 +0300

    Task52 in progress

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] array = new int[4, 4];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < array.GetLength())
