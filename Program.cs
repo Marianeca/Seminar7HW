d154e0a0592965266e936321226dc2bbfe648818
Mar <marianeca.of@gmail.com>
2022-12-13 16:23:30 +0300

    Task47 done

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+int m, n;
+
+Console.WriteLine($"Введите m размерность массива: ");
+int.TryParse(Console.ReadLine()!, out m);
+Console.WriteLine($"Введите n размерность массива: ");
+int.TryParse(Console.ReadLine()!, out n);
+
+double[,] array = new double[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+
